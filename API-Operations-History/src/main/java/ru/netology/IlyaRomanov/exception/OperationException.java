7b4fdc8dec1ac8fda000b8a7146204b19c4e50a8
AcryCxde <130891938+AcryCxde@users.noreply.github.com>
2023-12-23 15:30:40 +0500

    Delete API-Operations-History directory

--- API-Operations-History/src/main/java/ru/netology/IlyaRomanov/exception/OperationException.java
+++
@@ -1,6 +0,0 @@
-package ru.netology.IlyaRomanov.exception;
-
-public abstract class OperationException extends Exception {
-
-    public abstract String getMessage();
-}
--- API-Operations-History/src/main/java/ru/netology/IlyaRomanov/exception/OperationRuntimeException.java
+++
@@ -1,4 +0,0 @@
-package ru.netology.IlyaRomanov.exception;
-
-public class OperationRuntimeException extends RuntimeException {
-}
